9664bc57a2da20403c1a6d1cc1c5748f2d6aeffc
reyolan <reynardolan@gmail.com>
2021-12-09 20:51:25 +0800

    edit functions and loop 5 times

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,55 +1,48 @@
-// Computer picks between rock paper and scissors in an array
-function computerPlay() {
-    let compChoices = ["Rock", "Paper", "Scissors"];
-    return compChoices[Math.floor(Math.random() * compChoices.length)];
+function playerPlay() {
+    let playerSelect = prompt("Rock, Paper, or Scissors?");
+    return playerSelect.toLowerCase();
 }
 
+// Computer picks between rock paper and scissors in an array
+function computerPlay() {
+    let compChoices = ["rock", "paper", "scissors"];
+    return compChoices[Math.floor(Math.random() * compChoices.length)];
+};
+
 // Plays a single round of rock, paper and scissors
-
 function oneRound(playerSelection, computerSelection) {
-    // ask the user and to choose
-
-    let playerSelect = prompt("Rock, Paper, or Scissors?");
-
-    playerSelection = playerSelect.toLowerCase();
-
-    computerSelection = computerPlay().toLowerCase();
 
     if ((playerSelection === "rock" && computerSelection === "scissors") ||
         (playerSelection === "scissors" && computerSelection === "paper") ||
         (playerSelection === "paper" && computerSelection === "rock")) {
         alert(`You win! ${playerSelection} beats ${computerSelection}`);
+        return "1";
+
+
     } else if ((playerSelection === "rock" && computerSelection === "paper") ||
         (playerSelection === "scissors" && computerSelection === "rock") ||
         (playerSelection === "paper" && computerSelection === "scissors")) {
-        alert(`You lose! ${playerSelection} beats ${computerSelection}`);
+        alert(`You lose! ${playerSelection} loses to ${computerSelection}`);
+        return "0";
+
+
     } else if (playerSelection === computerSelection) {
-        alert(`It's a tie! ${playerSelection} ties with ${computerSelection}`);
+        return alert(`It's a tie! ${playerSelection} ties with ${computerSelection}`);
     } else {
-        return;
+        alert("Not a valid option!")
     }
-
-    /* Rock beats scissors
-    Rock loses to paper
-    Rock ties with rock
-    
-    Scissors beat paper
-    Scissors loses to rock
-    Scissors ties with scissors
-    
-    Paper beats rock
-    Paper loses to scissors
-    Paper ties with paper*/
-
 }
 
 
 function game() {
 
+    /*5 games of Rock, Paper, and Scissors*/
+    let userScore = 0;
+    let computerScore = 0;
 
-    oneRound();
-    oneRound();
-    oneRound();
-    oneRound();
-    oneRound();
+    for (let counter = 0; counter < 5; counter++)
+        oneRound(playerPlay(), computerPlay());
+
 }
+
+game();
