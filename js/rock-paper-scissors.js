ec7091aa68f08502130b1534053370ce6c7acad1
reyolan <reynardolan@gmail.com>
2021-12-09 22:55:39 +0800

    minor edit in functions

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,48 +1,68 @@
+let userScore = 0;
+let computerScore = 0;
+
 function playerPlay() {
-    let playerSelect = prompt("Rock, Paper, or Scissors?");
-    return playerSelect.toLowerCase();
+  let playerSelect = prompt("Rock, Paper, or Scissors?");
+  return playerSelect.toLowerCase();
 }
 
 // Computer picks between rock paper and scissors in an array
 function computerPlay() {
-    let compChoices = ["rock", "paper", "scissors"];
-    return compChoices[Math.floor(Math.random() * compChoices.length)];
+  let compChoices = ["rock", "paper", "scissors"];
+  return compChoices[Math.floor(Math.random() * compChoices.length)];
 };
 
-// Plays a single round of rock, paper and scissors
+// Plays a single round of rock, paper and scissors with score
 function oneRound(playerSelection, computerSelection) {
 
-    if ((playerSelection === "rock" && computerSelection === "scissors") ||
-        (playerSelection === "scissors" && computerSelection === "paper") ||
-        (playerSelection === "paper" && computerSelection === "rock")) {
-        alert(`You win! ${playerSelection} beats ${computerSelection}`);
-        return "1";
+  if ((playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "rock")) {
+    alert(`You win! ${playerSelection} beats ${computerSelection}`);
+    score("player")
 
+  } else if ((playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "scissors" && computerSelection === "rock") ||
+    (playerSelection === "paper" && computerSelection === "scissors")) {
+    alert(`You lose! ${playerSelection} loses to ${computerSelection}`);
+    score("computer")
 
-    } else if ((playerSelection === "rock" && computerSelection === "paper") ||
-        (playerSelection === "scissors" && computerSelection === "rock") ||
-        (playerSelection === "paper" && computerSelection === "scissors")) {
-        alert(`You lose! ${playerSelection} loses to ${computerSelection}`);
-        return "0";
+  } else if (playerSelection === computerSelection) {
+    alert(`It's a tie! ${playerSelection} ties with ${computerSelection}`);
+    score()
 
-
-    } else if (playerSelection === computerSelection) {
-        return alert(`It's a tie! ${playerSelection} ties with ${computerSelection}`);
-    } else {
-        alert("Not a valid option!")
-    }
+  } else {
+    alert("Not a valid option!");
+    score()
+  }
 }
 
+// Track score
+function score(winner) {
+  if (winner === "player") {
+    userScore += 1;
+    alert(`Player: ${userScore} Computer: ${computerScore}`)
+  } else if (winner === "computer") {
+    computerScore += 1;
+    alert(`Player: ${userScore} Computer: ${computerScore}`)
+  } else {
+    alert(`Player: ${userScore} Computer: ${computerScore}`)
+  }
+}
 
+/*5 rounds of Rock, Paper, and Scissors*/
 function game() {
 
-    /*5 games of Rock, Paper, and Scissors*/
-    let userScore = 0;
-    let computerScore = 0;
+  for (let counter = 0; counter < 5; counter++)
+    oneRound(playerPlay(), computerPlay());
 
-    for (let counter = 0; counter < 5; counter++)
-        oneRound(playerPlay(), computerPlay());
-
+  if (userScore > computerScore) {
+    return alert("You Win!")
+  } else if (userScore < computerScore) {
+    return alert("You lose!")
+  } else {
+    return alert("It's a tie!")
+  }
 }
 
 game();
