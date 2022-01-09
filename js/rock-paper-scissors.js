89046506785621f4a58f7e49a0ddc9d86d118812
Reynard Mark Olan <reynardolan@gmail.com>
2022-01-09 21:29:26 +0800

    reduce global variables

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,72 +1,87 @@
 let userScore = 0;
 let computerScore = 0;
 
-let choice = document.getElementsByTagName("button");
-let choiceArray = [...choice];
+function initializeEvents() {
+	let choice = document.getElementsByTagName("button");
+	let choiceArray = [...choice];
 
-choiceArray[0].addEventListener("click", () => funcChoice("Rock"));
-choiceArray[1].addEventListener("click", () => funcChoice("Paper"));
-choiceArray[2].addEventListener("click", () => funcChoice("Scissors"));
-choiceArray[3].addEventListener("click", () => playAgain());
-
-// appending new div for result
-let putResult = document.querySelector("body");
-let result = document.createElement("div");
-result.id = "result";
-
-let referenceNode = document.querySelector("#buttons");
-putResult.insertBefore(result, referenceNode);
-
-// appending p inside div for result
-let p = document.createElement("p");
-p.textContent = "Choose your weapon";
-p.id = "roundStatement";
-result.appendChild(p);
-
-// create a div for player
-let yourChoiceDiv = document.createElement("div");
-yourChoiceDiv.className = "choice";
-result.insertBefore(yourChoiceDiv, p);
-
-// create a div for computer
-let compChoiceDiv = document.createElement("div");
-compChoiceDiv.className = "choice";
-result.appendChild(compChoiceDiv);
-
-// appending p inside yourChoiceDiv for playerScore
-let yourScore = document.createElement("p");
-yourScore.textContent = `You: ${userScore}`;
-yourChoiceDiv.appendChild(yourScore);
-
-// appending p inside yourChoiceDiv for choice of player
-let yourChoice = document.createElement("p");
-yourChoice.textContent = " ";
-yourChoice.className = "choose";
-yourChoiceDiv.insertBefore(yourChoice, yourScore);
-
-// appending p inside compChoiceDiv for computerScore
-let compScore = document.createElement("p");
-compScore.textContent = `Computer: ${computerScore}`;
-compChoiceDiv.appendChild(compScore);
-
-// appending p inside compChoiceDiv for choice of computer
-let compChoice = document.createElement("p");
-compChoice.textContent = " ";
-compChoice.className = "choose";
-compChoiceDiv.insertBefore(compChoice, compScore);
-
-function funcChoice(choice) {
-	playRound(choice, playComputer());
+	choiceArray[0].addEventListener("click", () => funcChoice("Rock"));
+	choiceArray[1].addEventListener("click", () => funcChoice("Paper"));
+	choiceArray[2].addEventListener("click", () => funcChoice("Scissors"));
+	choiceArray[3].addEventListener("click", () => playAgain());
 }
 
-function playAgain() {
-	userScore = 0;
-	computerScore = 0;
-	p.textContent = "Choose your weapon";
+function initializeDom() {
+	// appending new div for result
+	let putResult = document.querySelector("body");
+	let result = document.createElement("div");
+	result.id = "result";
+
+	let referenceNode = document.querySelector("#buttons");
+	putResult.insertBefore(result, referenceNode);
+
+	// appending p inside div for result
+	let pRoundStatement = document.createElement("p");
+	pRoundStatement.textContent = "Choose your weapon";
+	pRoundStatement.id = "round-statement";
+	result.appendChild(pRoundStatement);
+
+	// create a div for player
+	let yourChoiceDiv = document.createElement("div");
+	yourChoiceDiv.className = "choice";
+	result.insertBefore(yourChoiceDiv, pRoundStatement);
+
+	// create a div for computer
+	let compChoiceDiv = document.createElement("div");
+	compChoiceDiv.className = "choice";
+	result.appendChild(compChoiceDiv);
+
+	// appending p inside yourChoiceDiv for playerScore
+	let yourScore = document.createElement("p");
+	yourScore.textContent = `You: 0`;
+	yourScore.id = "user-score";
+	yourChoiceDiv.appendChild(yourScore);
+
+	// appending p inside yourChoiceDiv for choice of player
+	let yourChoice = document.createElement("p");
 	yourChoice.textContent = " ";
+	yourChoice.className = "choose";
+	yourChoice.id = "user-choice";
+	yourChoiceDiv.insertBefore(yourChoice, yourScore);
+
+	// appending p inside compChoiceDiv for computerScore
+	let compScore = document.createElement("p");
+	compScore.textContent = `Computer: 0`;
+	compScore.id = "comp-score";
+	compChoiceDiv.appendChild(compScore);
+
+	// appending p inside compChoiceDiv for choice of computer
+	let compChoice = document.createElement("p");
 	compChoice.textContent = " ";
-	yourScore.textContent = `You: ${userScore}`;
-	compScore.textContent = `Computer: ${computerScore}`;
+	compChoice.className = "choose";
+	compChoice.id = "comp-choice";
+	compChoiceDiv.insertBefore(compChoice, compScore);
+}
+
+function updateScoreInDom(userScore, computerScore) {
+	let yourScore = document.querySelector("#user-score");
+	let compScore = document.querySelector("#comp-score");
+
+	yourScore.textContent = userScore;
+	compScore.textContent = computerScore;
+}
+
+function updateChoiceInDom(userChoice, computerChoice) {
+	let yourChoice = document.querySelector("#user-choice");
+	let compChoice = document.querySelector("#comp-choice");
+
+	yourChoice.textContent = userChoice;
+	compChoice.textContent = computerChoice;
+}
+
+function updateRoundStatement(roundStatement) {
+	let pRoundStatement = document.querySelector("#round-statement");
+	pRoundStatement.textContent = roundStatement;
 }
 
 // Computer picks between RPS
@@ -75,30 +90,52 @@ function playComputer() {
 	return compChoices[Math.floor(Math.random() * compChoices.length)];
 }
 
+function funcChoice(choice) {
+	playRound(choice, playComputer());
+}
+
+function playAgain() {
+	userScore = 0;
+	computerScore = 0;
+	document.querySelector("#round-statement").textContent = "Choose your weapon";
+	document.querySelector("#user-choice").textContent = " ";
+	document.querySelector("#comp-choice").textContent = " ";
+	document.querySelector("#user-score").textContent = `You: ${userScore}`;
+	document.querySelector(
+		"#comp-score"
+	).textContent = `Computer: ${computerScore}`;
+}
+
 // Plays a single round of rock, paper and scissors with score
 function playRound(playerSelection, computerSelection) {
-	yourChoice.textContent = `${playerSelection}`;
-	compChoice.textContent = `${computerSelection}`;
+	updateChoiceInDom(playerSelection, computerSelection);
+
 	if (
 		(playerSelection === "Rock" && computerSelection === "Scissors") ||
 		(playerSelection === "Scissors" && computerSelection === "Paper") ||
 		(playerSelection === "Paper" && computerSelection === "Rock")
 	) {
-		p.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
-		return trackScore("player");
+		updateRoundStatement(
+			`You win! ${playerSelection} beats ${computerSelection}`
+		);
+		trackScore("player");
 	} else if (
 		(playerSelection === "Rock" && computerSelection === "Paper") ||
 		(playerSelection === "Scissors" && computerSelection === "Rock") ||
 		(playerSelection === "Paper" && computerSelection === "Scissors")
 	) {
-		p.textContent = `You lose! ${playerSelection} loses to ${computerSelection}`;
+		updateRoundStatement(
+			`You lose! ${playerSelection} loses to ${computerSelection}`
+		);
 		return trackScore("computer");
 	} else if (playerSelection === computerSelection) {
-		p.textContent = `It's a tie! ${playerSelection} ties with ${computerSelection}`;
-		return trackScore();
+		updateRoundStatement(
+			`It's a tie! ${playerSelection} ties with ${computerSelection}`
+		);
+		trackScore();
 	} else {
-		p.textContent = "Not a valid option! Computer gets the score!";
-		return trackScore("computer");
+		roundStatement = "Not a valid option! Computer gets the score!";
+		trackScore("computer");
 	}
 }
 
@@ -106,16 +143,10 @@ function playRound(playerSelection, computerSelection) {
 function trackScore(winner) {
 	if (winner === "player") {
 		userScore += 1;
-		yourScore.textContent = `You: ${userScore}`;
-		compScore.textContent = `Computer: ${computerScore}`;
 	} else if (winner === "computer") {
 		computerScore += 1;
-		yourScore.textContent = `You: ${userScore}`;
-		compScore.textContent = `Computer: ${computerScore}`;
-	} else {
-		yourScore.textContent = `You: ${userScore}`;
-		compScore.textContent = `Computer: ${computerScore}`;
 	}
+	updateScoreInDom(`You: ${userScore}`, `Computer: ${computerScore}`);
 
 	if (userScore === 5 || computerScore === 5) {
 		declareWin();
@@ -125,15 +156,20 @@ function trackScore(winner) {
 // Declare win
 function declareWin() {
 	if (userScore === 5) {
-		p.textContent = "You Win!";
+		updateRoundStatement("You Win!");
 	} else {
-		p.textContent = "You lose!";
+		updateRoundStatement("You lose!");
 	}
 	disableButton();
 }
 
 function disableButton() {
+	let choice = document.getElementsByTagName("button");
+	let choiceArray = [...choice];
 	choiceArray[0].disabled = true;
 	choiceArray[1].disabled = true;
 	choiceArray[2].disabled = true;
 }
+
+initializeEvents();
+initializeDom();
